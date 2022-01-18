d8d627a1c8e4ac68dd3f6719aa115994990b95dc
blaskoasky <iriantomauduta@gmail.com>
2022-01-18 15:49:54 +0700

    adding RecycleViews for user reviews

---
+++ app/src/main/java/com/blaskoasky/iri/gps2/detail/ReviewAdapter.kt
@@ -0,0 +1,28 @@
+package com.blaskoasky.iri.gps2.detail
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.blaskoasky.iri.gps2.databinding.ItemReviewBinding
+import com.blaskoasky.iri.gps2.dto.MerchantEntity
+import java.util.*
+
+class ReviewAdapter : RecyclerView.Adapter<ReviewAdapter.ReviewViewHolder>() {
+
+    private var reviews = ArrayList<MerchantEntity>()
+
+    inner class ReviewViewHolder(private val binding: ItemReviewBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewViewHolder {
+        val binding = ItemReviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ReviewViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ReviewViewHolder, position: Int) {
+    }
+
+    override fun getItemCount(): Int = 15
+}
