fe83110a5db25b9d8ffb527b0934999d7f26ab93
Blaskoasky <53295055+blaskoasky@users.noreply.github.com>
2022-07-29 17:45:13 +0700

    splashscreen

---
+++ app/src/main/java/com/blaskoasky/iri/gps2/splash/SplashScreenActivity.kt
@@ -0,0 +1,24 @@
+package com.blaskoasky.iri.gps2.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.blaskoasky.iri.gps2.R
+import com.blaskoasky.iri.gps2.main.MainActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1500)
+    }
+}
