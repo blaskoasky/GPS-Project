d9474fb7706a190e8915cd1071b60ee90b67f163
Blaskoasky <53295055+blaskoasky@users.noreply.github.com>
2022-08-01 23:32:23 +0700

    tentang activity

--- app/src/main/java/com/blaskoasky/iri/gps2/activity/AboutAppActivity.kt
+++ app/src/main/java/com/blaskoasky/iri/gps2/activity/AboutAppActivity.kt
@@ -1,7 +1,9 @@
 package com.blaskoasky.iri.gps2.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.blaskoasky.iri.gps2.databinding.ActivityAboutAppBinding
 
 class AboutAppActivity : AppCompatActivity() {
@@ -14,5 +16,23 @@ class AboutAppActivity : AppCompatActivity() {
 
         actionBar?.setDisplayHomeAsUpEnabled(true)
         title = "About App"
+
+        binding.imgInstagram.setOnClickListener {
+            val instagram = "https://www.instagram.com/iriantomauduta/"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(instagram))
+            startActivity(intent)
+        }
+
+        binding.imgLinkedin.setOnClickListener {
+            val linkedin = "https://www.linkedin.com/in/iriantomauduta/"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(linkedin))
+            startActivity(intent)
+        }
+
+        binding.imgGithub.setOnClickListener {
+            val github = "https://github.com/blaskoasky/GPS-Project"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(github))
+            startActivity(intent)
+        }
     }
 }
